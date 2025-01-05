ddcc9ab4e6af9deec8818682c3a48a3808d669ec
hamo-o <yheel369@gmail.com>
2025-01-06 00:31:39 +0900

    style: 코드 포맷팅

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
 import Input from "./input.js";
 import Output from "./output.js";
-import {todoMap,  setTodoMap } from "./todoList.js";
+import { todoMap, setTodoMap } from "./todoList.js";
 
 class App {
-    async run() {
-        Output.printWelcome();
-        while(1) {
-            Output.printTodos();
-            Output.printMenu();
-    
-            const menu = await Input.readMenu();
-            if(menu === 'q' || menu === 'Q') {
-                Input.close();
-                break;
-            }
-            else if(menu === '1') {
-                await this.insert();
-            }
-            else if(menu === '2') {
-                await this.detail();
-            }
-            else if(menu === '3') {
-                await this.edit();
-            } 
-            else if (menu === '4') {
-                await this.delete();
-            }
-        }
-    }
+  async run() {
+    Output.printWelcome();
+    while (1) {
+      Output.printTodos();
+      Output.printMenu();
 
-    async insert() {
-        const {title, contents}= await Input.insertTodo();
-                setTodoMap({title, contents});
+      const menu = await Input.readMenu();
+      if (menu === "q" || menu === "Q") {
+        Input.close();
+        break;
+      } else if (menu === "1") {
+        await this.insert();
+      } else if (menu === "2") {
+        await this.detail();
+      } else if (menu === "3") {
+        await this.edit();
+      } else if (menu === "4") {
+        await this.delete();
+      }
     }
-    
-    async detail() {
-        const id = await Input.getTodoId();
-        Output.printTodoDetail(id);
-    }
+  }
 
-    async edit() {
-        const id = await Input.getTodoId();
-        const todo = todoMap.get(Number(id));
-        const {newTitle, newContents} = await Input.editTodo(todo.getDetail());
-        todo.editDetail(newTitle, newContents);
-    }
+  async insert() {
+    const { title, contents } = await Input.insertTodo();
+    setTodoMap({ title, contents });
+  }
 
-    async delete() {
-        const id = await Input.getTodoId();
-        todoMap.delete(Number(id));
-    }
+  async detail() {
+    const id = await Input.getTodoId();
+    Output.printTodoDetail(id);
+  }
+
+  async edit() {
+    const id = await Input.getTodoId();
+    const todo = todoMap.get(Number(id));
+    const { newTitle, newContents } = await Input.editTodo(todo.getDetail());
+    todo.editDetail(newTitle, newContents);
+  }
+
+  async delete() {
+    const id = await Input.getTodoId();
+    todoMap.delete(Number(id));
+  }
 }
 
 const app = new App();
--- todoList.js
+++ todoList.js
@@ -5,13 +5,13 @@ let index = 1;
 export const todoMap = new Map();
 
 export const setTodoMap = (todo) => {
-    const {title, contents} = todo;
-    const newTodo = new Todo(title, contents, index);
-    todoMap.set(index, newTodo);
-    index++;
+  const { title, contents } = todo;
+  const newTodo = new Todo(title, contents, index);
+  todoMap.set(index, newTodo);
+  index++;
 };
 
 export const isInTodoMap = (id) => {
-    if (todoMap.has(Number(id))) return true;
-    return false;
-}
+  if (todoMap.has(Number(id))) return true;
+  return false;
+};
